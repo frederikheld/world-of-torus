83ad909265aebd790d09ca5ef87241773b8a8211
Frederik Held <dev@frederikheld.de>
2017-11-09 17:15:13 +0100

    Can add Torlings to the TorusWorld now

--- test/torus-world-test.js
+++ test/torus-world-test.js
@@ -3,10 +3,11 @@
 var chai = require('chai')
 var expect = chai.expect
 var TorusWorld = require('./../lib/torus-world.js')
+var Torling = require('./../lib/torling.js')
 
 describe('TorusWorld', function() {
 
-	it('should be initialized correctly with positive values', function() {
+	it('should initialize size correctly', function() {
 		var size_x = 10
 		var size_y = 20
 
@@ -14,9 +15,10 @@ describe('TorusWorld', function() {
 
 		expect(torusWorld.getSizeX()).to.equal(size_x)
 		expect(torusWorld.getSizeY()).to.equal(size_y)
+
 	})
 
-	it('should throw an error if initialized with value that is not a positive integer', function() {
+	it('should throw an error if initialized with sizes lower than or equal to 0', function() {
 
 		// negative dimensions:
 		var surrogateNegative = function() {
@@ -29,8 +31,33 @@ describe('TorusWorld', function() {
 			new TorusWorld(0, 0)
 		}
 		expect(surrogateZero).to.throw(RangeError)
+
 	})
 
-	// TODO: You can't add two torlings with the torus-world!
+	it('should initialize tiles as undefined', function() {
+		var size_x = 10
+		var size_y = 20
+
+		var torusWorld = new TorusWorld(size_x, size_y)
+
+		expect(torusWorld.getItem(0, 0)).to.be.an('undefined')
+		expect(torusWorld.getItem(size_x - 1, size_y - 1)).to.be.an('undefined')
+	})
+
+	it('allows to put a torling onto a defined tile', function() {
+		var size_x = 10
+		var size_y = 20
+
+		var torusWorld = new TorusWorld(size_x, size_y)
+
+		torusWorld.putItem(0, 0, new Torling("Karlheinz"))
+		expect(torusWorld.getItem(0, 0) instanceof Torling)
+
+		torusWorld.putItem(9, 19, new Torling("Ute"))
+		expect(torusWorld.getItem(9, 19) instanceof Torling)
+
+	})
+
+	// TODO: You can't add two torlings with the same name to the torus-world!
 
 })
