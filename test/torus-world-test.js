798444f0e92f788875b02104d307621504c0eb52
Frederik Held <dev@frederikheld.de>
2017-11-09 19:02:18 +0100

    Refactored tests for TorusWorld
    Used beforeEach to define default vars

--- test/torus-world-test.js
+++ test/torus-world-test.js
@@ -7,15 +7,28 @@ var Torling = require('./../lib/torling.js')
 
 describe('TorusWorld', function() {
 
+	// Note: d contains default values that apply to all tests in this suite.
+	// I shortened it to a non self-explaining name, knowing that I'm breaking clean coding rules. But it's better for readability of the tests to have a short name.
+	var d
+
+	beforeEach(function() {
+
+		d = {}
+
+		d.size_x = 10
+		d.size_y = 20
+
+		d.torusWorld = new TorusWorld(d.size_x, d.size_y)
+
+	})
+
+	afterEach(function() {
+		d = null
+	})
+
 	it('should initialize size correctly', function() {
-		var size_x = 10
-		var size_y = 20
-
-		var torusWorld = new TorusWorld(size_x, size_y)
-
-		expect(torusWorld.getSizeX()).to.equal(size_x)
-		expect(torusWorld.getSizeY()).to.equal(size_y)
-
+		expect(d.torusWorld.getSizeX()).to.equal(d.size_x)
+		expect(d.torusWorld.getSizeY()).to.equal(d.size_y)
 	})
 
 	it('should throw an error if initialized with sizes lower than or equal to 0', function() {
@@ -32,29 +45,26 @@ describe('TorusWorld', function() {
 		}
 		expect(surrogateZero).to.throw(RangeError)
 
+		// The surrogates are neccessary in order to have the Exceptions thrown by TorusWorld passed correctly to expect()
+
 	})
 
 	it('should initialize tiles as undefined', function() {
-		var size_x = 10
-		var size_y = 20
-
-		var torusWorld = new TorusWorld(size_x, size_y)
-
-		expect(torusWorld.getItem(0, 0)).to.be.an('undefined')
-		expect(torusWorld.getItem(size_x - 1, size_y - 1)).to.be.an('undefined')
+		expect(d.torusWorld.getItem(0, 0)).to.be.an('undefined')
+		expect(d.torusWorld.getItem(d.size_x - 1, d.size_y - 1)).to.be.an('undefined')
 	})
 
 	it('allows to put a torling onto a defined tile', function() {
-		var size_x = 10
-		var size_y = 20
 
-		var torusWorld = new TorusWorld(size_x, size_y)
+		d.torusWorld.putItem(0, 0, new Torling("Karlheinz"))
+		expect(d.torusWorld.getItem(0, 0) instanceof Torling)
 
-		torusWorld.putItem(0, 0, new Torling("Karlheinz"))
-		expect(torusWorld.getItem(0, 0) instanceof Torling)
+		d.torusWorld.putItem(9, 19, new Torling("Ute"))
+		expect(d.torusWorld.getItem(9, 19) instanceof Torling)
 
-		torusWorld.putItem(9, 19, new Torling("Ute"))
-		expect(torusWorld.getItem(9, 19) instanceof Torling)
+	})
+
+	it('does not allow to put two Torlings with the same name into the TorusWorld', function() {
 
 	})
 
