651e5bc2295619fa3e9f27f7cb809daa905b3acd
Frederik Held <dev@frederikheld.de>
2017-11-12 16:22:23 +0100

    GUI can render current state of the world

--- server.js
+++ server.js
@@ -20,6 +20,21 @@ app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Create World:
+var TorusWorld = require('./lib/torus-world.js')
+var world = new TorusWorld(20, 10);
+
+// Populate world:
+var Torling = require('./lib/torling.js')
+
+var torling_herbert = new Torling("Herbert")
+torling_herbert.setColor('#ffcccc')
+world.putItem(4, 2, torling_herbert)
+
+var torling_tina = new Torling("Tina")
+torling_tina.setColor('#ffffcc')
+world.putItem(19, 9, torling_tina)
+
 // Communicate with clients:
 io.sockets.on("connection", function (socket) {
 
@@ -28,7 +43,8 @@ io.sockets.on("connection", function (socket) {
 	console.log(Date.now() + " [socket.io] SENDING \"welcome\" event to new client.");
 	socket.emit("welcome", {
 		"message": "Hello World!",
-		"client_id": client_id
+		"client_id": client_id,
+		"world_rendered": world.renderHTML()
 	});
 	client_id++;
 
